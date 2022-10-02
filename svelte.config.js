61560a09cec6c6aa5da2abb48a6dc790c9531e7e
Melvin A <melvinalmonte@gmail.com>
2022-10-02 12:42:13 -0400

    troubleshooting vercel static deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
-// uncomment line below for static deployment ie: deploy to s3, etc.
-// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,11 +12,9 @@ const config = {
 	],
 
 	kit: {
-		// uncomment line below when using static mode when deploying to s3
-		// adapter: adapter({
-		// 	fallback: 'index.html'
-		// })
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		})
 	}
 };
 
